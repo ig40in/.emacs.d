9b58cd3bccf2358e586f5d5e6d5312631107db77
Igor Sorokin <ig40in@gmail.com>
2024-06-29 13:40:30 -0400

    before working on Org mode

--- init.el
+++ init.el
@@ -7,7 +7,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(custom-safe-themes
+   '("34cf3305b35e3a8132a0b1bdf2c67623bc2cb05b125f8d7d26bd51fd16d547ec" default))
+ '(package-selected-packages
+   '(json-mode helm-lsp dap-mode apheleia flycheck company paredit helm-xref helm which-key avy hydra yasnippet projectile winum doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
